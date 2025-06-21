9d2dd27b3298c37157908eb734da2c863fe20264
Renaud11232 <gaspardrenaud@hotmail.com>
2025-06-21 14:16:37 +0200

    better handling of stored values if a nextcord object was removed

--- src/tonearm/bot/services/storage.py
+++ src/tonearm/bot/services/storage.py
@@ -28,26 +28,24 @@ class StorageService:
         with open(self.__file_name, 'w', encoding="utf-8") as f:
             json.dump(self.__data, f, indent=2)
 
-    async def __set(self, key: str | List[str], value):
+    def __set(self, key: str | List[str], value):
         key = self.__split_key(key)
-        async with self.__lock:
-            parent = self.__data
-            for k in key[:-1]:
-                if k not in parent:
-                    parent[k] = {}
-                parent = parent[k]
-            parent[key[-1]] = value
-            self.__save()
+        parent = self.__data
+        for k in key[:-1]:
+            if k not in parent:
+                parent[k] = {}
+            parent = parent[k]
+        parent[key[-1]] = value
+        self.__save()
 
-    async def __get(self, key: str | List[str], *, default=None) -> Any:
+    def __get(self, key: str | List[str], *, default=None) -> Any:
         key = self.__split_key(key)
-        async with self.__lock:
-            value = self.__data
-            for k in key:
-                if k not in value:
-                    return default
-                value = value[k]
-            return value
+        value = self.__data
+        for k in key:
+            if k not in value:
+                return default
+            value = value[k]
+        return value
 
     @staticmethod
     def __split_key(key: str | List[str]) -> List[str]:
@@ -56,36 +54,65 @@ class StorageService:
         return key
 
     async def get_volume(self) -> int:
-        return await self.__get("volume", default=100)
+        async with self.__lock:
+            return self.__get("volume", default=100)
 
     async def set_volume(self, volume: int):
-        await self.__set("volume", volume)
+        async with self.__lock:
+            self.__set("volume", volume)
 
     async def get_dj_roles(self) -> List[nextcord.Role]:
-        roles = await self.__get("dj.roles", default=[])
-        return [self.__guild.get_role(role) for role in roles]
+        async with self.__lock:
+            int_roles = self.__get("dj.roles", default=[])
+            roles = list(filter(lambda role: role is not None, map(self.__guild.get_role, int_roles)))
+            if len(int_roles) > len(roles):
+                self.__set_dj_roles(roles)
+            return roles
+
+    def __set_dj_roles(self, roles: List[nextcord.Role]):
+            int_roles = list(map(lambda role: role.id, roles))
+            self.__set("dj.roles", int_roles)
 
     async def set_dj_roles(self, dj_roles: List[nextcord.Role]):
-        roles = [role.id for role in dj_roles]
-        return await self.__set("dj.roles", roles)
+        async with self.__lock:
+            self.__set_dj_roles(dj_roles)
 
     async def get_dj_members(self) -> List[nextcord.Member]:
-        members = await self.__get("dj.members", default=[])
-        return [self.__guild.get_member(member) for member in members]
+        async with self.__lock:
+            int_members = self.__get("dj.members", default=[])
+            members = list(filter(lambda member: member is not None, map(self.__guild.get_member, int_members)))
+            if len(int_members) > len(members):
+                self.__set_dj_members(members)
+            return members
 
-    async def set_dj_members(self, dj_members: List[nextcord.Member]):
-        members = [member.id for member in dj_members]
-        return await self.__set("dj.members", members)
+    def __set_dj_members(self, members: List[nextcord.Member]):
+        int_members = list(map(lambda member: member.id, members))
+        self.__set("dj.members", int_members)
 
-    async def set_channel(self, channel: nextcord.TextChannel | None):
-        await self.__set("channel", None if channel is None else channel.id)
+    async def set_dj_members(self, members: List[nextcord.Member]):
+        async with self.__lock:
+            self.__set_dj_members(members)
 
     async def get_channel(self) -> nextcord.TextChannel | None:
-        channel = await self.__get("channel", default=None)
-        return None if channel is None else self.__guild.get_channel(channel)
+        async with self.__lock:
+            int_channel = self.__get("channel", default=None)
+            channel = self.__guild.get_channel(int_channel)
+            if int_channel is not None and channel is None:
+                self.__set_channel(None)
+            return channel
 
-    async def set_anarchy(self, anarchy: bool):
-        await self.__set("anarchy", anarchy)
+    def __set_channel(self, channel: nextcord.TextChannel | None):
+        int_channel = channel.id if channel is not None else None
+        self.__set("channel", int_channel)
+
+    async def set_channel(self, channel: nextcord.TextChannel | None):
+        async with self.__lock:
+            self.__set_channel(channel)
 
     async def get_anarchy(self) -> bool:
-        return await self.__get("anarchy", default=False)
+        async with self.__lock:
+            return self.__get("anarchy", default=False)
+
+    async def set_anarchy(self, anarchy: bool):
+        async with self.__lock:
+            self.__set("anarchy", anarchy)
