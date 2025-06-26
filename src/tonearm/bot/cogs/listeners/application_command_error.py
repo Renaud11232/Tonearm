89b7837b3975000349fb13ce36dac2ee630a770d
Renaud11232 <gaspardrenaud@hotmail.com>
2025-06-26 20:59:21 +0200

    update error message for checks

--- src/tonearm/bot/cogs/listeners/application_command_error.py
+++ src/tonearm/bot/cogs/listeners/application_command_error.py
@@ -23,7 +23,7 @@ class ApplicationCommandErrorListener(commands.Cog):
         if isinstance(error, ApplicationCheckFailure):
             await interaction.send(
                 ephemeral=True,
-                embed=self.__embed_service.error(error)
+                embed=self.__embed_service.error("You don't have permission to use this command.")
             )
         elif isinstance(error, nextcord.ApplicationInvokeError):
             exception = error.original
