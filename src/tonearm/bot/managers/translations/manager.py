bc64ac21c33fadda3b57b4dc257fd39eb18b3bb7
Renaud11232 <gaspardrenaud@hotmail.com>
2026-03-19 18:06:23 +0100

    dependencies updated & fixed translation paths

--- src/tonearm/bot/managers/translations/manager.py
+++ src/tonearm/bot/managers/translations/manager.py
@@ -4,16 +4,13 @@ from typing import Union
 
 from discord import Locale
 
-from injector import singleton, inject
+from injector import singleton
 
 from tonearm.bot.managers.base import ManagerBase
 
 
 @singleton
 class TranslationsManager(ManagerBase[Locale, Union[gettext.GNUTranslations, gettext.NullTranslations]]):
-
-    def __init__(self):
-        super().__init__()
 
     def _get_id(self, key: Locale) -> str:
         return key.value
