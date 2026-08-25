25318075c767ee208df05d9830259a2de96ae05b
Renaud GASPARD <gaspardrenaud@hotmail.com>
2026-08-25 07:33:55 +0200

    Improve ytdlp format selector again

--- src/tonearm/bot/services/source/youtube.py
+++ src/tonearm/bot/services/source/youtube.py
@@ -23,7 +23,7 @@ class YoutubeSourceService(SourceServiceBase):
     async def open(self, url: str) -> discord.AudioSource:
         self._logger.debug(f"Fetching media from YouTube URL : {url}")
         options = {
-            "format": "bestaudio/bestaudio*",
+            "format": "bestaudio[acodec=wav]/bestaudio/bestaudio*/best",
             "logger": self._logger,
             "js_runtimes": {
                 "deno": {}
